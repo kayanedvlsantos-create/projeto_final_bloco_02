5b18544ca5c0dec2032e2302b098c2e2b6c5fda5
kayanedvlsantos-create <kayanedvlsantos@gmail.com>
2026-04-16 14:24:06 -0300

    Fix: removendo o dto adicionado pelo comando nest g

--- src/categoria/controllers/categoria.controller.ts
+++ src/categoria/controllers/categoria.controller.ts
@@ -1,16 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CategoriaService } from '../categoria.service';
-import { CreateCategoriaDto } from './dto/create-categoria.dto';
-import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { CategoriaService } from '../services/categoria.service';
 
 @Controller('categoria')
 export class CategoriaController {
   constructor(private readonly categoriaService: CategoriaService) {}
-
-  @Post()
-  create(@Body() createCategoriaDto: CreateCategoriaDto) {
-    return this.categoriaService.create(createCategoriaDto);
-  }
 
   @Get()
   findAll() {
@@ -20,11 +13,6 @@ export class CategoriaController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.categoriaService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoriaDto: UpdateCategoriaDto) {
-    return this.categoriaService.update(+id, updateCategoriaDto);
   }
 
   @Delete(':id')
--- src/categoria/services/categoria.service.ts
+++ src/categoria/services/categoria.service.ts
@@ -1,12 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCategoriaDto } from './dto/create-categoria.dto';
-import { UpdateCategoriaDto } from './dto/update-categoria.dto';
 
 @Injectable()
 export class CategoriaService {
-  create(createCategoriaDto: CreateCategoriaDto) {
-    return 'This action adds a new categoria';
-  }
 
   findAll() {
     return `This action returns all categoria`;
@@ -14,10 +9,6 @@ export class CategoriaService {
 
   findOne(id: number) {
     return `This action returns a #${id} categoria`;
-  }
-
-  update(id: number, updateCategoriaDto: UpdateCategoriaDto) {
-    return `This action updates a #${id} categoria`;
   }
 
   remove(id: number) {
