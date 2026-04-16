3d7ae688e1d6c253f4236a3c717ce3a133192e47
kayanedvlsantos-create <kayanedvlsantos@gmail.com>
2026-04-16 14:15:20 -0300

    Configurando o Projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriaModule } from './categoria/categoria.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CategoriaModule],
+  imports: [
+      TypeOrmModule.forRoot({ //configuração do banco de dados
+      type: 'mysql', //tipo do banco de dados
+      host: 'localhost', //endereço do banco de dados
+      port: 3306, //porta do banco de dados
+      username: 'root', //  usuário do banco de dados
+      password: 'root', //senha do banco de dados
+      database: 'db_farmacia', //nome do banco de dados
+      entities: [], //entidades do banco de dados
+      synchronize: true, //sincroniza o banco de dados com as entidades, deve ser false em produção
+    }),
+
+    CategoriaModule,
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
